0395c3e62c023f03cd5f12a76205a637d482439f
Kristof-me <hk20021084@gmail.com>
2024-03-10 23:21:44 +0100

    Minor fixes

--- app/src/logarlec/App.java
+++ app/src/logarlec/App.java
@@ -1,9 +1,28 @@
 
 package logarlec;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        List<A> lista = new ArrayList<A>();
 
+        lista.add(new A<Integer>());
+        lista.add(new A<String>());
+
+        var a = lista.get(0).get(1);
+        var b = lista.get(1).get("a");
+
+        System.out.println(a.getClass());
+        System.out.println(b.getClass());
+
+    }
+
+    public static class A<T> {
+
+        public T get(T param) {
+            return param;
+        }
     }
 }
