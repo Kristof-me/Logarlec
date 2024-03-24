687b680d10b8f9b67373203248cfbb3152a58ee9
Benedek Bánáti <banatibenedek0@gmail.com>
2024-03-24 16:59:24 +0100

    Add defense strategy
    Do you know the definition of insanity?

--- app/src/logarlec/model/actor/strategy/DefenseStrategy.java
+++ app/src/logarlec/model/actor/strategy/DefenseStrategy.java
@@ -1,8 +1,10 @@
 package logarlec.model.actor.strategy;
 
+import logarlec.model.actor.Actor;
 import logarlec.model.items.Inventory;
 
 public abstract class DefenseStrategy {
+    protected Actor owner;
     public abstract boolean tick();
 
     public abstract boolean defend(Inventory inventory);
