a73642ea07137ba36666242c902f202134b0e63f
Horváth Kristóf <hk20021084@gmail.com>
2024-05-04 11:38:41 +0200

    Removed commands

--- app/src/logarlec/App.java
+++ app/src/logarlec/App.java
@@ -1,24 +1,16 @@
 
 package logarlec;
 
-import logarlec.control.Interpreter;
 import logarlec.view.frames.MenuFrame;
-
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 
 public class App {
     public static void main(String[] args) {
-        // try {
-        //     System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
-        // } catch (UnsupportedEncodingException e) {
-        //     throw new InternalError("VM does not support mandatory encoding UTF-8");
-        // }
-
-        // Interpreter.getInstance().handleInput(System.in);
         MenuFrame menuFrame = new MenuFrame();
         menuFrame.setVisible(true);
+    }
+
+    // ? lehet SRP miatt ezt nem ide kéne, de egy 1 függvényes osztályt nem akartam csinálni
+    private static void loadTheme() {
+
     }
 }
