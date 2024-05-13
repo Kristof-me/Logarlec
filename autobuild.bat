cd9e387f25687c7c5d38ba5f091f83d1d2bf0145
Horváth Kristóf <hk20021084@gmail.com>
2024-05-13 14:25:27 +0200

    Fixed autobuild
    Co-authored-by: Czeglédi Krisztián <SENPAITHATCHER@users.noreply.github.com>

--- autobuild.bat
+++ autobuild.bat
@@ -1,9 +1,12 @@
-@echo off
+:: @echo off
 chcp 65001
 
 :: Get the current directory
 set DIR=%~dp0
 cd %DIR%
+
+:: Copy resources
+xcopy /s /e /y .\app\src\resources .\app\bin\resources\
 
 :: Compile the source code
 javac -encoding "UTF-8"  -sourcepath .\app\src\ .\app\src\logarlec\App.java -d .\app\bin
