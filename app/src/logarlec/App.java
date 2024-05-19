81d8e559791ee777e1d144a56b69e497e3355e58
Czeglédi Krisztián <czegledi.krisz@edu.bme.hu>
2024-05-19 14:13:57 +0200

    Cleaned up main

--- app/src/logarlec/App.java
+++ app/src/logarlec/App.java
@@ -22,51 +22,6 @@ public class App {
         // Game setup
         MenuFrame menuFrame = new MenuFrame();
         menuFrame.setVisible(true);
-
-        
-
-        // Room r = new Room(10);
-        // Room r2 = new Room(10);
-        // Door d = new Door(r, r2, false);
-
-        // Student s = new Student();
-        // Player p = new Player(s);
-        // p.getActor().setName("asd");
-        // GameManager.getInstance().addPlayer(p);
-
-        // Student s2 = new Student();
-        // Player p2 = new Player(s2);
-        // p2.getActor().setName("bsd");
-        // GameManager.getInstance().addPlayer(p2);
-        
-        // r.getInventory().addItem(new AirFreshener());
-        // r.getInventory().addItem(new Tvsz());
-        
-        // Transistor a = new Transistor();
-        // s.getInventory().addItem(a);
-        // s.getInventory().addItem(new Transistor());
-        // s.use(a);
-        
-        
-
-        // Beer beer = new Beer();
-        // r.addItem(beer);
-        // s.teleport(r, false);
-        // s2.teleport(r, false);
-        // s.pickUp(beer);
-        //s.setActionState(new StunnedStep(s));
-        
-        //gameFrame.setVisible(true);
-        // p.takeTurn();
-        //s.use(beer);
-        // s.tick();
-        // s.tick();
-        // s.tick();
-        // s.tick();
-       // s.use(beer);
-        
-        //GameManager.getInstance().startGame();
-
         
     }
 }
